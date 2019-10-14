e734ccd435624e490ed1469dfa9092d1e36367e3
Jecé Xavier <jecexavier@gmail.com>
2019-10-14 09:58:50 -0300

    Criação do Trabalho em Java

---
+++ BuscaSegaInformada/src/buscasegainformada/control/Buscas.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buscasegainformada.control;
+
+/**
+ *
+ * @author Jece Xavier
+ */
+public class Buscas {
+    //Atributos
+    
+    //Contrutor
+    
+    //Minhas funções
+    
+    //Gets end Sets
+}
---
+++ BuscaSegaInformada/src/buscasegainformada/view/Tabuleiro.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buscasegainformada.view;
+
+import java.util.ArrayList;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author Jece Xavier
+ */
+public class Tabuleiro extends JFrame{
+    //Atributos
+    
+    
+    
+    
+    //Contrutor
+    
+    //Minhas funções
+    
+    //Gets end Sets
+}
